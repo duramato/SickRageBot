120904b2f13ba291d06d994ae2dc97deccd01262
Nils Vogels <bacardicoke@gmail.com>
2015-09-03 00:09:02 +0200

    Changing more text from CP to SR and tuning a bit

--- configs/issue.js
+++ configs/issue.js
@@ -4,34 +4,36 @@ module.exports = {
 	getNumber: function(data){
 		return data.issue.number;
 	},
-	before: 'Thanks for the issue report! To make sure issues are handled quickly, we use a simple checklist for you to fill out.
-                 Before a real human comes by, please make sure your report has all the below criteria checked',
-	after: 'Please make sure you also read [contribution guide](https://github.com/RuudBurger/CouchPotatoServer/blob/develop/contributing.md#issues) and followed all the steps. \n' +
-		'Make the title describe your issue. Having "SR not working" or "I get this bug" for 100 issues, isn\'t really helpful. We will close issues if there isn\'t enough information.' +
-		'Sometimes the devs may seem like a grunt and respond with short answers. This isn\'t (always) because the dev hates you, but because he\'s on mobile or busy fixing bugs. If something isn\'t clear, please let us know, and this bot may get updated to automatically answer you.' +
+	before: 'Thanks for the issue report! To make sure issues are handled quickly, it helps if all info is there. Before a real human comes by, please make sure your report has all the below criteria checked',
+	after: 'Please make sure you also read [how to create an issue](https://github.com/SiCKRAGETV/sickrage-issues/blob/master/README.md) and followed all the steps. \n\n' +
+		'Make the title describe your issue. Having "SR not working" or "I get this bug" for 100 issues, isn\'t really helpful. We will close issues if there isn\'t enough information.\n\n' +
+		'Sometimes the devs may seem like grunts and respond with short answers. This isn\'t (always) because the dev hates you, but because he\'s on mobile or busy fixing bugs. If something isn\'t clear, please let us know, and this bot may get updated to automatically answer you.' +
 		'\n\nThanks!',
 	checks: [
 
-		// Fill in the dots
+		// Basic info
 		{
-			message: 'Fill in all the dots. They\'re there for a reason',
+			message: 'Include basic information: Branch/Commit, OS, What you did, What happened, What you expected',
 			condition: function (data) {
-				var body = data.issue.body.toLowerCase();
-
-				var is_sr_log = 1;
-				_.each(['information:', 'reproduce:', 'logs:'], function(check_for){
-					is_sr_log += _.contains(body, check_for) ? 1 : 0;
-				});
-
-				var forgot_filled_in = _.contains(data.issue.body, ': ...');
-
-				return is_cp_log && !forgot_filled_in;
+				return 0
 			}
 		},
 
+                // Enable debugging
+                {
+                        message: 'Enable debug logging (be sure to disable after the bug is fixed)',
+                        condition: function (data) {
+                                var inline_logs = isInlineLog(data.issue.body);
+                                if(inline_logs)
+                                        return _.contains(data.issue.body, 'DEBUG [');
+                                return 0;
+                        }
+                },
+
+
 		// Post logs
 		{
-			message: 'Post logs, either inline (for smaller logs) or using gist)',
+			message: 'Post debug logs, either inline (for smaller logs) or using gist)',
 			condition: function (data) {
 
 				var inline_logs = isInlineLog(data.issue.body);
@@ -53,34 +55,6 @@ module.exports = {
 
 				if(inline_logs)
 					return _.contains(data.issue.body, '```');
-			}
-		},
-
-		// Enable debugging
-		{
-			message: 'Enable debug logging (be sure to disable after the bug is fixed)',
-			condition: function (data) {
-				var inline_logs = isInlineLog(data.issue.body);
-				if(inline_logs)
-					return _.contains(data.issue.body, 'DEBUG [');
-			}
-		},
-
-		// Version information missing
-		{
-			message: 'Post full version information',
-			condition: function (data) {
-
-				var has_version = _.contains(data.issue.body, 'Branch'),
-					is_desktop = false,
-					correct_repo = false;
-
-				// Make sure logs are from correct repo
-				// if(has_version){
-				//correct_repo = _.contains(data.issue.body, 'SiCKRAGETV:');
-				// }
-
-				return correct_repo || is_desktop;
 			}
 		}
 
